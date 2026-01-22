43d1dea6b779fe827efba63de4ee0d3a09b91086
oofyneril <lrwong369@gmail.com>
2026-01-22 18:18:00 +0800

    Complete Level-2

--- src/main/java/Peggy.java
+++ src/main/java/Peggy.java
@@ -1,7 +1,9 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Peggy {
     public static void main(String[] args) {
+        ArrayList<String> txts = new ArrayList<>();
         System.out.println("---------------------------------------------");
         System.out.println("Hello! I'm Peggy\n" + "What can I do for you?");
         System.out.println("---------------------------------------------");
@@ -13,9 +15,18 @@ public class Peggy {
                 System.out.println("Bye. Hope to see you again soon!");
                 System.out.println("---------------------------------------------");
                 break;
-            } else {
+            } else if (inputName.equals("list")) {
+                int count = 0;
                 System.out.println("---------------------------------------------");
-                System.out.println(inputName);
+                for (String s : txts) {
+                    count+=1;
+                    System.out.println(count + ". " + s);
+                }
+                System.out.println("---------------------------------------------");
+            } else {
+                txts.add(inputName);
+                System.out.println("---------------------------------------------");
+                System.out.println("added: " + inputName);
                 System.out.println("---------------------------------------------");
             }
         }
