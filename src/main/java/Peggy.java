7720704e6e5b67b61926e0e3b15f922d4b28daab
oofyneril <lrwong369@gmail.com>
2026-01-22 18:03:47 +0800

    Update Level-0

--- src/main/java/Peggy.java
+++ src/main/java/Peggy.java
@@ -2,18 +2,18 @@ import java.util.Scanner;
 
 public class Peggy {
     public static void main(String[] args) {
-        String name = "Peggy";
-        System.out.println("Hello! I'm " + name + "\n" + "What can I do for you?");
-        while(true) {
-            Scanner input = new Scanner(System.in);
-            String inputName = input.nextLine();
-            if (inputName.equals("bye")) {
-                System.out.println("Bye. Hope to see you again soon!");
-                break;
-            }
-        }
-
-
-
+        System.out.println("----------------------------------------------------");
+        System.out.println("Hello! I'm " + "Peggy" + "\n" + "What can I do for you?");
+        System.out.println("----------------------------------------------------");
+        System.out.println("Bye. Hope to see you again soon!");
+        System.out.println("----------------------------------------------------");
+//        while(true) {
+//            Scanner input = new Scanner(System.in);
+//            String inputName = input.nextLine();
+//            if (inputName.equals("bye")) {
+//
+//                break;
+//            }
+//        }
     }
 }
