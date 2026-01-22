2c03c19501ec206e28b03ef6d9a5a178c8134eca
oofyneril <lrwong369@gmail.com>
2026-01-22 22:37:45 +0800

    Complete Level-4

--- src/main/java/Event.java
+++ src/main/java/Event.java
@@ -9,6 +9,6 @@ public class Event extends Task {
     }
 
     public String toString() {
-        return "[E]" + super.toString() + " (from: " + this.fromTime + ", to: " + this.toTime + ")";
+        return "[E]" + super.toString() + " (from: " + this.fromTime + " to: " + this.toTime + ")";
     }
 }
